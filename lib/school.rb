87605f43e10e6ee8b86267a44d4c1bf64e432e35
“zed” <zed@kolk.me>
2021-01-13 17:44:47 -0600

    commit tree c3ca5564cd3f99a5a56cc2cda580e7645b15864d

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+    def initialize school=nil
+        @school = school
+        @roster = {}
+    end
+    attr_accessor :school
+
+    def roster
+      @roster
+    end
+    
+    def add_student student, grade
+      if @roster.has_key?(grade) == false
+        @roster[grade] = []
+      end
+      @roster[grade] << student
+    end
+    def grade num
+      @roster[num]
+    end
+    def sort
+      sorted = @roster.map do |key, value|
+        [key, value.sort]
+      end
+      sorted.to_h
+    end
+end
